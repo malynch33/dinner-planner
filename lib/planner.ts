72932b260ca0a3c89c3941501f768109fe92b622
malynch33 <malynch33@gmail.com>
2026-03-01 12:54:12 -0500

    Fix suffle to only one any

--- lib/planner.ts
+++ lib/planner.ts
@@ -16,7 +16,7 @@ function getCurrentWeekMonday() {
   return monday;
 }
 
-function shuffle(items: any[]): any[] {
+function shuffle(items: any[]) {
   const arr = [...items];
   for (let i = arr.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
